1091f891155b919e4f771c477106ae6fead27380
iovse <gkgrh8vtwf@privaterelay.appleid.com>
2026-06-13 12:43:46 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+// Service worker mínimo para soporte offline
+const CACHE = 'bupa-crm-v2';
+const ASSETS = [
+  './',
+  './Agentes BUPA v2.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{})));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  // No interceptar llamadas a APIs externas (Google, etc.)
+  if (url.origin !== location.origin) return;
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
+      return resp;
+    }).catch(() => caches.match('./Agentes BUPA v2.html')))
+  );
+});
